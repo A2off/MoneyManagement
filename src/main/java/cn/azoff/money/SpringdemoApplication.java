14c157231d6bfc02214e3602484758b8b28f7785
Azoff <azoff@azoff.cn>
2020-02-20 14:34:00 +0800

    Update Setting
    Signed-off-by: Azoff <azoff@azoff.cn>

--- src/main/java/cn/azoff/money/SpringdemoApplication.java
+++ src/main/java/cn/azoff/money/SpringdemoApplication.java
@@ -2,17 +2,18 @@ package cn.azoff.money;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages={"cn.azoff.money"})
-@MapperScan("cn.azoff.money.**.dao")
+@ComponentScan(basePackages={"cn.azoff"})
+@MapperScan("cn.azoff.money.*.dao")
+//@AutoConfigureMybatis
 public class SpringdemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringdemoApplication.class, args);
     }
-
+    
 }
